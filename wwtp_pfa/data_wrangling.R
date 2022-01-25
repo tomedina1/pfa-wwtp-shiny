191e4e2f03dac957bd5b9d51a2bf1edce84154bf
tomedinabren <tomedina@bren.ucsb.edu>
2022-01-25 14:30:08 -0800

    started data analysis

---
+++ wwtp_pfa/data_wrangling.R
@@ -0,0 +1,60 @@
+
+library(tidyverse)
+library(here)
+library(janitor)
+
+
+### Hyperion WWTP (Los Angeles)
+hyperion <- read_csv(here('wwtp_pfa','data','hyperion.csv')) %>% 
+  clean_names() %>% 
+  select(samp_date, field_pt_name, parameter:value, units) %>% 
+  filter(field_pt_name %in% c('5-MILE', 'RI-NORS', 'RI-NCOS', 'RI-NOS', 'RI-CWIS', 'RI-COS'),
+         parvq == '=',
+         units == 'NG/L') %>% 
+  mutate(wwtp = 'Los Angeles - Hyperion')
+
+  for(i in 1:length(hyperion$field_pt_name)){
+  
+    if(hyperion$field_pt_name[i] == '5-MILE'){
+      hyperion$field_pt_name[i] = 'effluent'}
+  
+    else{
+      hyperion$field_pt_name[i] = 'influent'}}
+
+
+### San Jose Creek WRP (Whittier)
+whittier <- read_csv(here('wwtp_pfa', 'data', 'whittier.csv')) %>% 
+  clean_names() %>% 
+  select(samp_date, field_pt_name, parameter:value, units) %>% 
+  filter(field_pt_name %in% c('WN_DCL_TER', 'WN_RAW'),
+         parvq == '=',
+         units == "NG/L") %>% 
+  mutate(wwtp = 'Whittier - San Jose Creek')
+
+  for(i in 1:length(whittier$field_pt_name)){
+  
+    if(whittier$field_pt_name[i] == 'WN_DCL_TER'){
+      whittier$field_pt_name[i] = 'effluent'}
+  
+    else{
+      whittier$field_pt_name[i] = 'influent'}}
+
+
+### Tillman (Los Angeles)
+tillman <- read_csv(here('wwtp_pfa', 'data', 'tillman.csv')) %>% 
+  clean_names() %>% 
+  select(samp_date, field_pt_name, parameter:value, units) %>% 
+  filter(field_pt_name %in% c('PW', 'RI'),
+         parvq == '=',
+         units == 'NG/L') %>% 
+  mutate(wwtp = 'Los Angeles - Tillman')
+
+  for(i in 1:length(tillman$field_pt_name)){
+  
+    if(tillman$field_pt_name[i] == 'PW'){
+      tillman$field_pt_name[i] = 'effluent'}
+  
+    else{
+      tillman$field_pt_name[i] = 'influent'}}
+
+  
