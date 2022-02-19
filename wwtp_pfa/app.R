2155e9d98bdaceb790cf1bd136e3e0a6c9c3ac8e
tomedinabren <tomedina@bren.ucsb.edu>
2022-02-18 16:09:36 -0800

    edited info on background page

--- wwtp_pfa/app.R
+++ wwtp_pfa/app.R
@@ -5,6 +5,7 @@ library(leaflet)
 library(shinyWidgets)
 library(bslib)
 library(plotly)
+library(writexl)
 
 
 source('data_wrangling.R')
@@ -16,27 +17,58 @@ my_theme <- bs_theme(bootswatch = "lux")
 
 
 ui <- fluidPage(theme = my_theme,
-              
+
                 
                 navbarPage('PFA Tracker',
                            
                            tabPanel('Background',
-                                    includeMarkdown('background.md'),
+                                    
+                                   
                                     sidebarLayout(
                                       sidebarPanel(width = 3,
+                                                   
+                                                   h3('Additional PFA Info'),
+                                                   
+                                                   tags$div('Click below for additional detailed PFAs info from the',
+                                                            tags$a(href="https://www.oecd.org/chemicalsafety/portal-perfluorinated-chemicals/", "OECD"), '.'),
+                                                   
+                                                   br(),
+                                                   
+                                                   downloadButton('dl', 'Download Additional PFA information'),
+                                                   
+                                                   br(),
+                                                   br(),
+                                                   
+                                                   p('This dataset contains additional information on PFA structures, naming conventions, regulatory status,
+                                                     historical production data, chemical sources, and ecological and human effects.'),
+                                                   
+                                                   br(),
+                                                   
+                                                   tags$div('For additional information on the chemical and physical properties of PFAs and other chemicals, visit',
+                                                            tags$a(href = "https://pubchem.ncbi.nlm.nih.gov/", "PubChem"), '.'),
+                                                   
+                                                   
+                                                   hr(style = "border-top: 1px solid #000000;"),
                                                 
-                                                  sliderInput("mass_range",
+                                                   sliderInput("mass_range",
                                                               label = h3('Select a mass range'),
                                                               min = 210,
                                                               max = 815,
                                                               value = c(390, 610),
                                                               ticks = FALSE,
-                                                              sep = "")
+                                                              sep = ""),
+                                                   
+                                                   p('This slider selects a range of molar masses (g/mol) to display the PFAs being studied that have masses in the selected range.')
+
                                                   
                                                   ),
                                       
                                       
+                                      
+                                      
                                       mainPanel(
+                                        includeMarkdown('background.md'),
+                                        
                                         dataTableOutput("pfadt")
                                       )
                                       
@@ -136,6 +168,21 @@ ui <- fluidPage(theme = my_theme,
 server <- function(input, output, session){
   
   ### BACKGROUND
+  
+  output$dl <- downloadHandler(
+    
+    filename = function(){
+      'pfa_add_info.xlsx'
+    },
+    
+    content = function(file){
+      write_xlsx(
+        'pfa_add_info.xlsx'
+      )
+    }
+  )
+  
+  
   output$pfadt <- renderDataTable({
     dt <- parameters[parameters$molar_mass >= input$mass_range[1] & parameters$molar_mass <= input$mass_range[2], ]},
   
